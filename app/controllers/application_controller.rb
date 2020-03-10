dac4fb250fedafdeeb8f87e417b264804440b38b
Nick Semerad <nicksemerad@gmail.com>
2020-03-09 20:55:15 -0600

    end of class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  # def user_params
+  #   params.require(:user).permit(:email, :password)
+  # end
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :user
+  validates :balance, numericality: {greater_than_or_equal_to: 25,
+   message: "Accounts have a minimum of $25"
+  }
 end
